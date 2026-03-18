55417dd320291397df121aafb73013b319917bb8
Purab <balanipb@gmail.com>
2026-03-18 02:29:50 -0700

    successfully finished pnr and setting up gate sim

---
+++ step1/gate_sim/xcelium.d/run.lnx8664.20.09.d/.timestamp.ts
@@ -0,0 +1,3 @@
+1773822199 /home/linux/ieng6/ECE260B_WI26_A00/pbalani/project/Dual-core-machine-learning-accelerator-for-attention-mechanism/step1/gate_sim/netlist/fullchip_tb.v
+1773821913 /home/linux/ieng6/ECE260B_WI26_A00/pbalani/project/Dual-core-machine-learning-accelerator-for-attention-mechanism/step1/gate_sim/netlist/tcbn65gplus.v
+1773822168 /home/linux/ieng6/ECE260B_WI26_A00/pbalani/project/Dual-core-machine-learning-accelerator-for-attention-mechanism/step1/gate_sim/netlist/fullchip.pnr.v
