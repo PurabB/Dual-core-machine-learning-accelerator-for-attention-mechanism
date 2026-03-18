dc2c0f8c14839aff274516a7c83ad35a772288fb
Purab <balanipb@gmail.com>
2026-03-18 03:12:05 -0700

    step 1 finished

--- step1/gate_sim/xcelium.d/run.lnx8664.20.09.d/files.ts
+++ step1/gate_sim/xcelium.d/run.lnx8664.20.09.d/files.ts
@@ -1,3 +1,6 @@
+1773233315 /home/linux/ieng6/ECE260B_WI26_A00/pbalani/project/Dual-core-machine-learning-accelerator-for-attention-mechanism/step1/syn/verilog/fifo_mux_2_1.v
+1773233315 /home/linux/ieng6/ECE260B_WI26_A00/pbalani/project/Dual-core-machine-learning-accelerator-for-attention-mechanism/step1/syn/verilog/fifo_mux_8_1.v
+1773233315 /home/linux/ieng6/ECE260B_WI26_A00/pbalani/project/Dual-core-machine-learning-accelerator-for-attention-mechanism/step1/syn/verilog/fifo_mux_16_1.v
 1773822168 /home/linux/ieng6/ECE260B_WI26_A00/pbalani/project/Dual-core-machine-learning-accelerator-for-attention-mechanism/step1/gate_sim/netlist/fullchip.pnr.v
 1773821913 /home/linux/ieng6/ECE260B_WI26_A00/pbalani/project/Dual-core-machine-learning-accelerator-for-attention-mechanism/step1/gate_sim/netlist/tcbn65gplus.v
-1773822199 /home/linux/ieng6/ECE260B_WI26_A00/pbalani/project/Dual-core-machine-learning-accelerator-for-attention-mechanism/step1/gate_sim/netlist/fullchip_tb.v
+1773828003 /home/linux/ieng6/ECE260B_WI26_A00/pbalani/project/Dual-core-machine-learning-accelerator-for-attention-mechanism/step1/gate_sim/netlist/fullchip_tb.v
