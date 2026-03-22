bcc72258b66afe3ae28143b556d1f4b2200a91bf
Purab Balani <balanipb@gmail.com>
2026-03-21 23:33:42 -0700

    added step 1 & 2

---
+++ step1/pnr/innovus.tcl
@@ -0,0 +1,8 @@
+source loadDesignTech.tcl
+source initialFloorplan.tcl
+source placement.tcl
+source pinPlacement.tcl
+source clock.tcl
+source route.tcl
+source reportDesign.tcl
+source outputGen.tcl
